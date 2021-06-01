4a81c24a5226598631ab85d70019f294476836ce
Cherish Truong <groovu@gmail.com>
2021-06-01 10:48:57 -0700

    Uploading VS proj files

---
+++ Models/AvailableContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Availibility.Models
+{
+    public partial class AvailableContext : DbContext
+    {
+        public AvailableContext()
+        {
+        }
+
+        public AvailableContext(DbContextOptions<AvailableContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Model> Model { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Available;integrated security=True;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Model>(entity =>
+            {
+                entity.ToTable("model");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Location)
+                    .IsRequired()
+                    .HasColumnName("location")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Notes)
+                    .HasColumnName("notes")
+                    .HasMaxLength(1000)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Time)
+                    .HasColumnName("time")
+                    .HasColumnType("datetime");
+            });
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Availibility.Models
+{
+    public partial class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public string Location { get; set; }
+        public string Notes { get; set; }
+    }
+}
