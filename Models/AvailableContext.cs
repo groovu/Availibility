eb8f2eeaa57c13a89375239d08786dca8b34f079
Cherish Truong <truong@berkeley.edu>
2021-06-12 16:37:02 -0700

    removed string warning

--- Models/AvailableContext.cs
+++ Models/AvailableContext.cs
@@ -21,7 +21,6 @@ namespace Availibility.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Available;integrated security=True;Trusted_Connection=True;");
             }
         }
